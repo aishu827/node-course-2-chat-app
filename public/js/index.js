48201e258a907ac88c4ff72c0f2b54a30e622b1e
aishwarya <aishu827@gmail.com>
2019-02-15 14:53:36 +0530

    'scrolling'

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages=jQuery('#messages');
+  var newMessage=messages.children('li:last-child');
+  var clientHeight=messages.prop('clientHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -9,24 +23,29 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  var formattedTime= moment(message.createdAt).format('h:mm a');
+  var template=jQuery('#message-template').html();
+  var html=Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
-//edit geolocation2
 socket.on('newLocationMessage',function(message){
-  var li=jQuery('<li></li>');
-  var a=jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from}: `);
-  a.attr('href',message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var formattedTime= moment(message.createdAt).format('h:mm a');
+  var template=jQuery('#location-message-template').html();
+  var html=Mustache.render(template,{
+    from:message.from,
+    url:message.url,
+    createdAt:formattedTime
+  });
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
-//end of edit geolocation2
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
@@ -41,7 +60,6 @@ jQuery('#message-form').on('submit', function (e) {
   });
 });
 
-//edit geolocation-1
 var locationButton=jQuery('#send-location');
 locationButton.on('click',function(){
   if(!navigator.geolocation){
